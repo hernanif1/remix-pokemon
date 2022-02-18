dcd0e436b8007f41f506c5456b0919b0f93fe9bc
Hernani Fernandes <hernanif1@gmail.com>
2022-02-18 17:30:40 +0100

    remove netlify code

--- remix.config.js
+++ remix.config.js
@@ -2,9 +2,7 @@
  * @type {import('@remix-run/dev').AppConfig}
  */
 module.exports = {
-  serverBuildTarget: "netlify",
-  server: "./server.js",
-  ignoredRouteFiles: [".*"]
+  ignoredRouteFiles: [".*"],
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
   // serverBuildPath: "build/index.js",
