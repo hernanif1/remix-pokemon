b03a86f10a945d7df3e138276311c86db721f590
Hernani Fernandes <hernanif1@gmail.com>
2022-02-18 17:59:54 +0100

    improve not found page

--- app/pages/Error.tsx
+++ app/pages/Error.tsx
@@ -11,13 +11,20 @@ export default function Error() {
         <Links />
       </head>
       <body>
-        <div className="w-full h-full bg-black flex justify-center items-center text-white">
-          <div className="text-center">
+        <div className="w-full h-full bg-red-800 flex justify-center items-center text-white">
+          <div className="flex flex-col text-center">
+            <img
+              className="self-center"
+              src="https://user-images.githubusercontent.com/1771727/154727379-53a56d67-4bae-4dd1-bb6e-7b33d9f3852b.png"
+              width={300}
+            />
             <h1 className="mb-4 text-7xl font-bold text-center">
-              Something went wrong :( <br />
-              {caught.status} {caught.statusText}
+              Something went wrong <br />
             </h1>
-            <Link className="poke-button" to="/">
+            <p className="mb-4 text-5xl font-bold text-center text-red-300">
+              {caught.status} {caught.statusText}
+            </p>
+            <Link className="poke-button py-5" to="/">
               go to home
             </Link>
           </div>
